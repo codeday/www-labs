93eb73215cb2754dc5c2a998c10bfef08d41338f
Tyler Menezes <github@xanadu.ws>
2020-06-12 17:47:31 -0400

    Add caching, trycatch airtable fetch

--- src/utils/airtable.js
+++ src/utils/airtable.js
@@ -1,5 +1,6 @@
 import getConfig from 'next/config';
 import Airtable from 'airtable';
+import moment from 'moment-timezone';
 
 const { serverRuntimeConfig } = getConfig();
 
@@ -14,10 +15,19 @@ export const fetchAll = async (select) => {
   return allRecords;
 };
 
+let cachedCalendar = null;
+let lastCachedAt = null;
 export const getCalendar = async () => {
  const table = new Airtable({ apiKey: serverRuntimeConfig.airtable.key })
   .base(serverRuntimeConfig.airtable.base)
   .table(serverRuntimeConfig.airtable.table);
 
-  return await fetchAll(table.select({ fields: ['Title', 'Date', 'Type', 'Speakers'] }));
+  if (!lastCachedAt || moment().subtract(1, 'minute').isAfter(lastCachedAt)) {
+    lastCachedAt = moment();
+    try {
+      cachedCalendar = await fetchAll(table.select({ fields: ['Title', 'Date', 'Type', 'Speakers'] }));
+    } catch (err) { return }
+  };
+
+  return cachedCalendar;
 }
