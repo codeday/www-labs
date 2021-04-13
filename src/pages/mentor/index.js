71a322d5a0656846f2fa4e2114e785f050316001
Tyler Menezes <tylermenezes@codeday.org>
2021-04-13 15:52:45 -0400

    Add emoji to share button

--- src/pages/mentor/index.js
+++ src/pages/mentor/index.js
@@ -75,7 +75,7 @@ export default function Mentor() {
               FAQs
             </Button>
             <Button as="a" href="mailto:labs@codeday.org" variant="ghost" variantColor="green">Email Us</Button>
-            <Button as="a" href="/mentor/share" variant="ghost" variantColor="green">Share With Your Company</Button>
+            <Button as="a" href="/mentor/share" variant="ghost" variantColor="green">💌 Share With Your Company</Button>
           </Box>
           <Box backgroundColor="red.50" padding={6} marginTop="-2rem">
             <Heading as="h3" size="lg" paddingBottom={2}>Timeline</Heading>
