a473eef6be736e091c973a8c04aeb3dc859f0943
Tyler Menezes <tylermenezes@codeday.org>
2021-05-25 09:58:17 -0400

    Add proposal submission

--- src/components/Dashboard/ProjectEditor.js
+++ src/components/Dashboard/ProjectEditor.js
@@ -7,12 +7,11 @@ import { default as Textarea } from '@codeday/topo/Atom/Input/Textarea';
 import Text, { Heading } from '@codeday/topo/Atom/Text';
 import { useToasts } from '@codeday/topo/utils';
 import { useFetcher } from '../../dashboardFetch';
-import { EditProject } from './ProjectEditor.gql';
+import { EditProject, ProposeProject } from './ProjectEditor.gql';
 
-export default function ProjectEditor({ project, ...rest }) {
+export default function ProjectEditor({ project: originalProject, ...rest }) {
+  const [project, setProject] = useState(originalProject);
   const [loading, setLoading] = useState(false);
-  const [description, setDescription] = useState(project.description);
-  const [deliverables, setDeliverables] = useState(project.deliverables);
   const fetch = useFetcher();
   const { success, error } = useToasts();
 
@@ -26,39 +25,64 @@ export default function ProjectEditor({ project, ...rest }) {
 
       <Heading as="h4" fontSize="lg">Description</Heading>
       <Textarea
-        value={description}
+        value={project.description}
         placeholder="2-3 sentence description of what your students will work on."
-        onChange={(e) => setDescription(e.target.value)}
+        onChange={(e) => setProject({ ...project, description: e.target.value })}
       />
 
       <Heading mt={4} as="h4" fontSize="lg">Deliverables</Heading>
       <Textarea
-        value={deliverables}
+        value={project.deliverables}
         placeholder="~3 milestones you expect your students to meet."
-        onChange={(e) => setDeliverables(e.target.value)}
+        onChange={(e) => setProject({ ...project, deliverables: e.target.value })}
       />
 
-      <Button
-        mt={4}
-        onClick={async () => {
-          setLoading(true);
-          try {
-            await fetch(print(EditProject), {
-              id: project.id,
-              description: description || "",
-              deliverables: deliverables || ""
-            });
-            success('Project updated.');
-          } catch (ex) {
-            error(ex.toString());
-          }
-          setLoading(false);
-        }}
-        loading={loading}
-        disabled={loading}
-      >
-        Save
-      </Button>
+      <Box mt={4}>
+        <Button
+          d="inline-block"
+          onClick={async () => {
+            setLoading(true);
+            try {
+              const result = await fetch(print(EditProject), {
+                id: project.id,
+                description: project.description || "",
+                deliverables: project.deliverables || ""
+              });
+              setProject(result.labs.editProject);
+              success('Project updated.');
+            } catch (ex) {
+              error(ex.toString());
+            }
+            setLoading(false);
+          }}
+          loading={loading}
+          disabled={loading}
+        >
+          Save
+        </Button>
+        {project.status === 'DRAFT' && (
+          <Box d="inline-block" ml={2} pl={2} borderLeftWidth={1}>
+            <Button
+              d="inline-block"
+              onClick={async () => {
+                setLoading(true);
+                try {
+                  const result = await fetch(print(ProposeProject), { id: project.id });
+                  setProject(result.labs.editProject);
+                  success('Project submitted');
+                } catch (ex) {
+                  error(ex.toString());
+                }
+                setLoading(false);
+              }}
+              loading={loading}
+              disabled={loading || !project.description || !project.deliverables}
+            >
+              {!project.description || !project.deliverables ? 'Fill All Fields to Propose' : 'Submit Proposal'}
+            </Button>
+          </Box>
+        )}
+      </Box>
     </Box>
   )
 }
