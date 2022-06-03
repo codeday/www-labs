3bcdb31fcc8d8f745e2c048aceab2b7a18ebbb0f
Tyler Menezes <tylermenezes@codeday.org>
2022-06-03 17:30:34 -0400

    Update mentoring start date when on July 4

--- src/providers/programDates.js
+++ src/providers/programDates.js
@@ -4,9 +4,13 @@ import { useQuery } from './query';
 const wrapDt = (dict) => Object.keys(dict).reduce((accum, k) => ({ ...accum, [k]: DateTime.fromISO(dict[k]) }), {});
 export const useProgramDates = () => {
   const dates = wrapDt(useQuery('cms.programDates.items.0' || {}));
+  let mentoringStartsAt = dates.startsAt.plus({ weeks: 1 });
+  if (mentoringStartsAt.month === 7 && mentoringStartsAt.day === 4) {
+    mentoringStartsAt = mentoringStartsAt.plus({ days: 1});
+  }
   return {
     ...dates,
-    mentoringStartsAt: dates.startsAt.plus({ weeks: 1 }),
+    mentoringStartsAt,
     extendedProgramEndsAt: dates.startsAt.plus({ weeks: 12 }),
     mentorApplicationEndsAt: dates.startsAt.plus({ weeks: -2 }),
     mentorApplicationFocusAt: dates.startsAt.plus({ weeks: -6 }),
