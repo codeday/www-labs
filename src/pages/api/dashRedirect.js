f58fdae141ec87013b7c26d358edc5d6623ebd19
Tyler Menezes <tylermenezes@codeday.org>
2022-06-10 10:46:49 -0400

    Add evt to token

--- src/pages/api/dashRedirect.js
+++ src/pages/api/dashRedirect.js
@@ -3,13 +3,14 @@ import getConfig from 'next/config';
 import { sign } from 'jsonwebtoken';
 import { getSession } from 'next-auth/client';
 import { apiFetch } from '@codeday/topo/utils';
-import { StudentMentorQuery } from './dashRedirect.gql';
+import { StudentMentorQuery, NextEventQuery } from './dashRedirect.gql';
+import { DateTime } from 'luxon';
 
 const { serverRuntimeConfig } = getConfig();
 
-function makeToken ({ typ, sid, tgt }) {
+function makeToken ({ typ, sid, tgt, evt }) {
   return sign(
-    { typ, sid, tgt },
+    { typ, sid, tgt, evt },
     serverRuntimeConfig.gql.secret,
     { audience: serverRuntimeConfig.gql.audience, expiresIn: '31d', noTimestamp: true }
   );
@@ -19,8 +20,12 @@ export default async function (req, res) {
   const session = await getSession({ req });
   if (!session?.user?.nickname) return res.send(null);
 
+  const { cms } = await apiFetch(print(NextEventQuery), { now: DateTime.now().toISO() });
+  const evt = cms?.events?.items?.[0]?.id;
+  if (!evt) return res.send(null);
+
   const username = session.user.nickname;
-  const adminToken = makeToken({ typ: 'a' });
+  const adminToken = makeToken({ typ: 'a', evt });
   const accountToken = sign({ scopes: `read:users` }, serverRuntimeConfig.gql.accountSecret, { expiresIn: '5m' });
 
   const { labs, account } = await apiFetch(
@@ -39,10 +44,10 @@ export default async function (req, res) {
 
   const tokens = {
     a: isAdmin && adminToken,
-    mm: (isAdmin || isManager) && makeToken({ typ: 'mm', sid: username, tgt: 'u' }),
-    r: (isAdmin || isReviewer) && makeToken({ typ: 'r', sid: username, tgt: 'u' }),
-    m: labs?.mentor?.id && makeToken({ typ: 'm', sid: labs.mentor.id, tgt: 'i' }),
-    s: labs?.student?.id && makeToken({ typ: 's', sid: labs.student.id, tgt: 'i' }),
+    mm: (isAdmin || isManager) && makeToken({ typ: 'mm', sid: username, tgt: 'u', evt }),
+    r: (isAdmin || isReviewer) && makeToken({ typ: 'r', sid: username, tgt: 'u', evt }),
+    m: labs?.mentor?.id && makeToken({ typ: 'm', sid: labs.mentor.id, tgt: 'i', evt }),
+    s: labs?.student?.id && makeToken({ typ: 's', sid: labs.student.id, tgt: 'i', evt }),
   };
 
   return res.send(Object.keys(tokens)
