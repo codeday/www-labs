55e1fa57677009682bf31dba01243cb9216600b5
Tyler Menezes <x@tyler.vc>
2024-01-22 14:05:33 -0800

    s/mentor/peer/

--- src/utils/getReflectionType.js
+++ src/utils/getReflectionType.js
@@ -1,6 +1,6 @@
 export function getReflectionType(sr, s) {
   if (sr.project) return 'project';
-  if (!(sr.authorMentor || sr.authorStudent)) return sr.mentor ? 'mentee' : 'mentor';
+  if (!(sr.authorMentor || sr.authorStudent)) 'peer';
   if ((sr.authorMentor || sr.authorStudent).id === s.id) return 'self';
   if (sr.mentor && sr.authorStudent) return 'mentee';
   if (sr.student && sr.authorMentor) return 'mentor';
